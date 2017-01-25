d5605785473ea7be60fc75f746530a1ac14399c4
AlexYakovis <yakovis@boylesoftware.com>
2017-01-25 17:27:58 -0500

    add my code for realm and  loopback 3

--- index.js
+++ index.js
@@ -1,122 +1,163 @@
+/**
+ * Created by alexander on 1/25/17.
+ */
 'use strict';
 
 var supertest = require('supertest');
 var async = require('async');
 
+
 module.exports = {
-  run: function(conf, app, url, callback) {
-    var server;
-    var agent = supertest.agent(url);
-    var baseURL = '/';
 
-    if (typeof conf !== 'object') {
-      return callback('Failed to load test configuration from file');
-    }
+    run: function (conf, app, url, callback) {
 
-    if (app) {
-      before(function(done) {
-        server = app.listen(done);
-      });
+        var loginUrl = 'api/customers/login';
+        var logOutUrl = 'api/customers/logout';
 
-      after(function(done) {
-        server.close(done);
-      });
-    }
+        var server;
+        var agent = supertest.agent(url);
+        var baseURL = '/';
 
-    describe('Loopback API', function() {
-      async.each(conf, function(c, asyncCallback) {
-        if (!c.hasOwnProperty('method')) {
-          callback('Test has no method specified');
-          return asyncCallback();
+        if (typeof conf !== 'object') {
+            return callback('Failed to load test configuration from file');
         }
 
-        if (!c.hasOwnProperty('model')) {
-          callback('Test has no route specified');
-          return asyncCallback();
-        }
-
-        if (!c.hasOwnProperty('expect')) {
-          callback('Test has no expected response code specified');
-          return asyncCallback();
-        }
-
-        var hasData = (c.hasOwnProperty('withData'));
-        var isWithAuthentication = (c.hasOwnProperty('username') && c.hasOwnProperty('password'));
-        var authenticationDescription = (isWithAuthentication) ? 'authenticated' : 'unauthenticated';
-
-        var description = 'should respond '+c.expect+' on '+authenticationDescription+' '+c.method+' requests to /'+c.model;
-        var parsedMethod;
-        var loginBlock;
-
-        if (c.method.toUpperCase() === 'GET') {
-          parsedMethod = agent.get(baseURL+c.model);
-        } else if (c.method.toUpperCase() === 'POST') {
-          parsedMethod = agent.post(baseURL+c.model);
-        } else if (c.method.toUpperCase() === 'PUT') {
-          parsedMethod = agent.put(baseURL+c.model);
-        } else if (c.method.toUpperCase() === 'DELETE') {
-          parsedMethod = agent.delete(baseURL+c.model);
-        } else if (c.method.toUpperCase() === 'PATCH') {
-          parsedMethod = agent.patch(baseURL+c.model);
-        } else {
-          callback('Test has an unrecognized method type');
-          return asyncCallback();
-        }
-
-        if (isWithAuthentication) {
-          loginBlock = function(loginCallback) {
-            agent
-            .post(baseURL+'users/login')
-            .send({ email: c.username, password: c.password, ttl: '1209600000' })
-            .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
-            .expect(200)
-            .end(function(err, authRes) {
-              if (err) {
-                return loginCallback('Could not log in with provided credentials', null);
-              }
-
-              var token = authRes.body.id;
-
-              return loginCallback(null, token);
+        if (app) {
+            before(function (done) {
+                server = app.listen(done);
             });
-          }
-        } else {
-          loginBlock = function(loginCallback) { 
-            return loginCallback(null, null); 
-          };
+
+            after(function (done) {
+                server.close(done);
+            });
         }
 
-        it(description, function(done) {
-          loginBlock(function(loginError, loginToken) {
-            if (loginError) {
-              done(loginError);
-              return asyncCallback();
-            }
+        describe('Loopback API', function () {
+            async.each(conf, function (c, asyncCallback) {
 
-            if (loginToken) {
-              parsedMethod = parsedMethod.set('Authorization', loginToken);
-            }
+                if (!c.hasOwnProperty('method')) {
+                    callback('Test has no method specified');
+                    return asyncCallback();
+                }
 
-            if (hasData) {
-              parsedMethod = parsedMethod.send(c.withData)
-              .set('Content-Type', 'application/json');
-            }
+                if (!c.hasOwnProperty('model')) {
+                    callback('Test has no route specified');
+                    return asyncCallback();
+                }
 
-            parsedMethod
-            .expect(c.expect)
-            .end(function(err, res) {
-              if (err) {
-                done(err);
-                return asyncCallback();
-              } else {
-                done();
-                return asyncCallback();
-              }
+                if (!c.hasOwnProperty('expect')) {
+                    callback('Test has no expected response code specified');
+                    return asyncCallback();
+                }
+
+                var hasData = (c.hasOwnProperty('withData'));
+                var hasPathValues = (c.hasOwnProperty('withPathValues'));
+                var isWithAuthentication = (c.hasOwnProperty('email') && c.hasOwnProperty('password') && c.hasOwnProperty('realm'));
+                var authenticationDescription = (isWithAuthentication) ? 'authenticated' : 'unauthenticated';
+
+                var description = 'should respond ' + c.expect + ' on ' + authenticationDescription + ' ' + c.method + ' requests to /' + c.model;
+                var parsedMethod;
+                var loginBlock;
+
+
+                if (isWithAuthentication) {
+                    loginBlock = function (loginCallback) {
+                        agent
+                            .post(baseURL + loginUrl)
+                            .send({email: c.email, password: c.password, realm: c.realm, ttl: '1209600000'})
+                            .set('Accept', 'application/json')
+                            .set('Content-Type', 'application/json')
+                            .expect(200)
+                            .end(function (err, authRes) {
+                                if (err) {
+                                    return loginCallback('Could not log in with provided credentials', null);
+                                }
+
+                                var token = authRes.body.id;
+                                var currentUserId = authRes.body.userId;
+                                return loginCallback(null, token, currentUserId);
+                            });
+                    };
+                } else {
+                    loginBlock = function (loginCallback) {
+                        return loginCallback(null, null, null);
+                    };
+                }
+
+                var logOut = function (token, logOutCallback) {
+                    agent
+                        .post(baseURL + logOutUrl + '?access_token=' + token)
+                        .set('Accept', 'application/json')
+                        .set('Content-Type', 'application/json')
+                        .expect(200)
+                        .end(function (err) {
+                            if (err) {
+                                return logOutCallback('Could not log out');
+                            }
+                            return logOutCallback(null);
+                        });
+                };
+
+                it(description, function (done) {
+                    loginBlock(function (loginError, loginToken, currentUserId) {
+                        if (loginError) {
+                            done(loginError);
+                            return asyncCallback();
+                        }
+
+                        if (hasPathValues) {
+                            c.model = c.model + '/' + currentUserId
+                        }
+
+                        if (c.method.toUpperCase() === 'GET') {
+                            parsedMethod = agent.get(baseURL + c.model);
+                        } else if (c.method.toUpperCase() === 'POST') {
+                            parsedMethod = agent.post(baseURL + c.model);
+                        } else if (c.method.toUpperCase() === 'PUT') {
+                            parsedMethod = agent.put(baseURL + c.model);
+                        } else if (c.method.toUpperCase() === 'DELETE') {
+                            parsedMethod = agent.delete(baseURL + c.model);
+                        } else if (c.method.toUpperCase() === 'PATCH') {
+                            parsedMethod = agent.patch(baseURL + c.model);
+                        } else {
+                            callback('Test has an unrecognized method type');
+                            return asyncCallback();
+                        }
+
+
+                        if (loginToken) {
+                            parsedMethod.query({access_token: loginToken});
+                            //parsedMethod = parsedMethod.set('Authorization', loginToken);
+                        }
+
+
+                        if (hasData) {
+                            parsedMethod = parsedMethod.send(c.withData)
+                                .set('Content-Type', 'application/json');
+                        }
+
+                        parsedMethod
+                            .expect(c.expect)
+                            .end(function (err, res) {
+                                if (err) {
+                                    done(err);
+                                } else {
+                                    if (c.login == true) {
+                                        loginToken = res.body.id;
+                                    }
+                                    done();
+                                }
+                                if (loginToken) {
+                                    logOut(loginToken, function (err) {
+                                        //NO OPS
+                                    });
+                                }
+
+                                return asyncCallback();
+                            });
+                    });
+                });
             });
-          });
         });
-      });
-    });
-  }
-}
+    }
+};
--- readme.md
+++ readme.md
@@ -2,24 +2,27 @@
 [![npm](https://img.shields.io/npm/dm/loopback-api-testing.svg)]()
 [![npm](https://img.shields.io/npm/l/loopback-api-testing.svg)]()
 
-This package is a simplified replacement for [loopback-testing](https://github.com/strongloop/loopback-testing), which is now considered deprecated. It generates [Mocha](https://mochajs.org/) tests for Loopback API routes and examines their responses.
+This package is a simplified replacement for [loopback-testing](https://github.com/strongloop/loopback-testing), which is now considered deprecated.
+It generates [Mocha](https://mochajs.org/) tests for Loopback API routes and examines their response codes.
+Optimized for Loopback 3 with realm
 
-The main difference between this package and [loopback-testing](https://github.com/strongloop/loopback-testing) is that loopback-api-testing does not require you to write any code. Tests are specified in json format and the tests are generated automatically.
+The main difference between this package and [loopback-testing](https://github.com/strongloop/loopback-testing) is that loopback-api-testing does not require you to write any code.
+Tests are specified in json format and the tests are generated automatically.
 
 This package is not supported by, endorsed by, or associated with Strongloop or the core Loopback team.
 
-## Installing ##
-
-```bash
-npm install loopback-api-testing
-```
+[//]: # ## Installing ##
+[//]: #
+[//]: # ```bash
+[//]: # npm install loopback-api-testing
+[//]: # ```
 
 ## Example Usage ##
 
 The test file (e.g. `test/loopbackAPI.test.js`)
 
 
-```js
+```jsSimplified automated API testing for Loopback 3 with realm
 var loopbackApiTesting = require('loopback-api-testing');
 var tests = require('./apiTestConfig.json');
 var server = require('../server/server.js');
@@ -53,7 +56,142 @@ mocha --reporter spec test
 Should get you:
 
 ```
-  Loopback API
+  Loopback API[
+  {
+    "method": "GET",
+    "model": "status",
+    "expect": 200
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/login",
+    "expect": 400
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/login",
+    "withData": {
+      "realm": "VROOM"
+    },
+    "expect": 400
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/login",
+    "withData": {
+      "email": "fake",
+      "password": "fake"
+    },
+    "expect": 400
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/login",
+    "withData": {
+      "realm": "VROOM",
+      "email": "fake",
+      "password": "fake"
+    },
+    "expect": 401
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/",
+    "withData": {
+      "FirstName": "TestUser",
+      "LastName": "TestUser",
+      "EmailAddress": "TestUser@TestUser.com",
+      "PhoneNumber": "917",
+      "password": "TestUser@TestUser.com",
+      "EmailMarketing": false,
+      "realm": "SUYC",
+      "email": "TestUser@TestUser.com"
+    },
+    "expect": 200
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/",
+    "withData": {
+      "FirstName": "TestUser",
+      "LastName": "TestUser",
+      "EmailAddress": "TestUser@TestUser.com",
+      "PhoneNumber": "917",
+      "password": "TestUser@TestUser.com",
+      "EmailMarketing": false,
+      "realm": "SUYC",
+      "email": "TestUser@TestUser.com"
+    },
+    "expect": 422
+  },
+  {
+    "method": "POST",
+    "model": "api/customers/login",
+    "withData": {
+      "realm": "SUYC",
+      "email": "TestUser@TestUser.com",
+      "password": "TestUser@TestUser.com"
+    },
+    "expect": 200,
+    "login": true
+  },
+  {
+    "method": "GET",
+    "model": "api/customers/validToken",
+    "password": "TestUser@TestUser.com",
+    "realm": "SUYC",
+    "email": "TestUser@TestUser.com",
+    "expect": 200
+  },
+  {
+    "method": "PATCH",
+    "model": "api/customers/",
+    "password": "TestUser@TestUser.com",
+    "realm": "SUYC",
+    "email": "TestUser@TestUser.com",
+    "withPathValues": [
+      "{currentUserId}"
+    ],
+    "withData": {
+      "FirstName": "NewName2"
+    },
+    "expect": 200
+  },
+  {
+    "method": "DELETE",
+    "model": "api/customers/",
+    "password": "TestUser@TestUser.com",
+    "realm": "SUYC",
+    "email": "TestUser@TestUser.com",
+    "withPathValues": [
+      "{currentUserId}"
+    ],
+    "expect": 200
+  },
+  {
+    "method": "GET",
+    "model": "status",
+    "expect": 200,
+    "nextTests": [
+      {
+        "method": "POST",
+        "model": "Customers",
+        "withData": {
+          "FirstName": "TestUser",
+          "LastName": "TestUser",
+          "EmailAddress": "TestUser@TestUser.com",
+          "PhoneNumber": "917",
+          "password": "TestUser@TestUser.com",
+          "EmailMarketing": false,
+          "realm": "SUYC",
+          "email": "TestUser@TestUser.com"
+        },
+        "expect": 200
+      }
+    ]
+  }
+]
+
     ✓ should respond 401 on unauthenticated GET requests to /Users (67ms)
 
 
